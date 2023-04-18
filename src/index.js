4a4befd6fa87379f9b4d5850b73fa09d7c131898
sharon kessio <sharonkessio6@gmail.com>
2023-04-18 22:54:25 +0300

    Revert "React tutorial"

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import TodoContainer from './components/TodoContainer';
 
-// const element = <h1>Hello from Create React App</h1>
+
+//const element = <h1>Hello from Create React App</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TodoContainer />
-
-  </React.StrictMode>,
+     <TodoContainer />
+    
+  </React.StrictMode>
 );
+
+
